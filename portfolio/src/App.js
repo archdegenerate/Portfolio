16c196b611ad9ddde5a36d734bb1b20ccf1c0b9c
Tomáš Mäsiar <tomi.masiar@gmail.com>
2024-05-07 10:54:48 +0200

    initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+      <div className='h-10 w-10 bg-red-500' />
+    </h1>
+  );
+}
+
+export default App;
