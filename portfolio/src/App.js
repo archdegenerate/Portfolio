e537e51bd546882f7e575aa68f21225d7841ab80
Tomáš Mäsiar <tomi.masiar@gmail.com>
2024-05-23 13:25:15 +0200

    add about component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
+import Hero from './components/Hero';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-      <div className='h-10 w-10 bg-red-500' />
-    </h1>
+    <Hero/>
   );
 }
 
